710d7df6d4d3e2bbb21802aecb523cbea8a9303a
jbenper <j.ben.perry@gmail.com>
2024-11-23 21:41:47 -0500

    changed to exception and added ASCII art

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from utilities.models import SaveFile
 from utilities.general_utils import print_attributes, arguments
 
 if __name__ == "__main__":
+    print(r"""
+          
+  _______          ____  __    _____                 
+ |  __ \ \        / /  \/  |  / ____|                
+ | |  | \ \  /\  / /| \  / | | (___   __ ___   _____ 
+ | |  | |\ \/  \/ / | |\/| |  \___ \ / _` \ \ / / _ \
+ | |__| | \  /\  /  | |  | |  ____) | (_| |\ V /  __/
+ |_____/   \/  \/   |_|  |_| |_____/ \__,_| \_/ \___|
+                                                                                              
+""")
     file_loc: str = arguments.file
 
     file_name: str = file_loc.split("/")[-1]
@@ -9,9 +19,9 @@ if __name__ == "__main__":
     with open(file_loc, "rb") as save_file:
         save_bytes: list[int] = [int_byte for int_byte in (save_file.read())]
     
-    assert(len(save_bytes) == 8192)
+    if len(save_bytes) != 8192:
+        raise Exception("Save File Length does not match .sav format")
 
     save = SaveFile(file_name=file_name, save_ints=save_bytes)
 
     print(save)
-
